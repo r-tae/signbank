e501bc10fe03083e16414821d8d9279256cace89
River Tae Smith <22485304+r-tae@users.noreply.github.com>
2024-02-28 00:05:32 +1100

    Add instructions to disable LAN access to dev server

--- config/dev.exs
+++ config/dev.exs
@@ -17,8 +17,7 @@ config :signbank, Signbank.Repo,
 # watchers to your application. For example, we can use it
 # to bundle .js and .css sources.
 config :signbank, SignbankWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
+  # Change to `ip: {127, 0, 0, 1}` to prevent access from other machines.
   http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
